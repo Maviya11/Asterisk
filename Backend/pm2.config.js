cd6ea3293401e479a85ba00478887d64094de11d
Maviya_11 <maviyaakhtar8@gmail.com>
2025-02-19 03:21:23 +0530

    configured backend for deployment

--- Backend/pm2.config.js
+++ Backend/pm2.config.js
@@ -1,14 +1,12 @@
-module.exports = {
-  apps: [
-    {
-      name: "profile-logic",
-      script: "profile-logic.js",
-      watch: false,
-    },
-    {
-      name: "recurring-logic",
-      script: "recurring-logic.js",
-      watch: false,
-    },
-  ],
-};
+export const apps = [
+  {
+    name: "profile-logic",
+    script: "profile-logic.js",
+    watch: false,
+  },
+  {
+    name: "recurring-logic",
+    script: "recurring-logic.js",
+    watch: false,
+  },
+];
